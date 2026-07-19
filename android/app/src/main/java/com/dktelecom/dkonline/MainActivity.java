142480054b0f5a14b5217d787c8eca6b2f25139f
lokojhow <discipulojonathas@gmail.com>
2026-07-19 10:55:35 -0300

    Adicionar projeto Android

---
+++ android/app/src/main/java/com/dktelecom/dkonline/MainActivity.java
@@ -0,0 +1,12 @@
+package com.dktelecom.dkonline;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(RouterBridgePlugin.class);
+    }
+}
